f389ffa1279898c6f5c3c26a25c115826ec28eac
Pietro Sette <pietrosette@gmail.com>
2020-09-01 10:29:40 +0200

    inserimento festivi done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 var dataInizio = moment('2018-01-01');
 
 insertDays(dataInizio);
+insertHolidays(dataInizio);
 
 
 // $('h1.month').html(dataCorrente.format('MMM') + ' ' + dataCorrente.format('YYYY'));
@@ -30,7 +31,7 @@ function insertDays(data){
 
             month: month,
 
-            completeDate: year + '-' + month + '-' + addZero(i)
+            completeDate: year + '-' + data.format('MM') + '-' + addZero(i)
         };
 
         console.log(context);
@@ -40,6 +41,29 @@ function insertDays(data){
         $('.month-list').append(html);
     }
 }
+
+function insertHolidays(data){
+    $.ajax({
+        url: 'https://flynn.boolean.careers/exercises/api/holidays',
+        method: 'GET',
+        data: {
+            year: data.year(),
+            month: data.month()
+        },
+        success: function(risposta){
+            for (var i = 0; i < risposta.response.length; i++){
+                var listItem = $('li[data-completa="'+ risposta.response[i].date + '"]');
+                listItem.append('-' + risposta.response[i].name);
+                listItem.addClass('festivo');
+                console.log(listItem);
+            }
+        }
+
+
+
+    });
+}
+
 
 function addZero(n){
     if(n < 10){
